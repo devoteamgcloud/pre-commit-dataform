6e2fb63cd2ffb2d44f5a0136315ade51e9db9d9b
Hugo Rialan <hugo.rialan@devoteamgcloud.com>
2023-06-29 13:51:30 +0200

    v1 fonctionnelleé

--- hooks/dataform_format.sh
+++ hooks/dataform_format.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+# Check if dataform command is available
+if ! command -v dataform >/dev/null 2>&1; then
+  echo "Error: dataform command not found. Make sure it is installed and available in PATH."
+  exit 1
+fi
+
+# Run Dataform formatting for SQLX files
 dataform format
+
+exit 0
