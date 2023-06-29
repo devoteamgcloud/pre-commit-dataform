7cc0ac9b019c28f0c92018204413e7ac98b1c23d
Hugo Rialan <hugo.rialan@devoteamgcloud.com>
2023-06-29 13:13:07 +0200

    fix

--- hooks/dataform_format.sh
+++ hooks/dataform_format.sh
@@ -6,21 +6,7 @@ if ! command -v dataform >/dev/null 2>&1; then
   exit 1
 fi
 
-# Get the list of staged files
-files=$(git diff --cached --name-only --diff-filter=ACM | grep '\.df$')
-
-# Check if there are any Dataform files to format
-if [ -z "$files" ]; then
-  echo "No Dataform files to format. Skipping pre-commit hook."
-  exit 0
-fi
-
-# Run Dataform formatting on each file
-echo "Formatting Dataform files..."
-for file in $files; do
-  dataform fmt $file
-  git add $file
-done
+dataform format
 
 echo "Dataform formatting complete. Please review the changes before committing."
 exit 0
