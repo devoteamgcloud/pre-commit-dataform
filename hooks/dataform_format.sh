4d751a538bd8cd52c1f434438c7dadc9feaa9f1b
Hugo Rialan <hugo.rialan@devoteamgcloud.com>
2023-06-29 13:21:06 +0200

    fix

--- hooks/dataform_format.sh
+++ hooks/dataform_format.sh
@@ -1,20 +1,3 @@
 #!/bin/sh
 
-# Check if dataform command is available
-if ! command -v dataform >/dev/null 2>&1; then
-  echo "Error: dataform command not found. Make sure it is installed and available in PATH."
-  exit 1
-fi
-
-# Run Dataform formatting
-output=$(dataform format 2>&1)
-exit_code=$?
-
-if [ $exit_code -ne 0 ]; then
-  echo "Dataform formatting failed:"
-  echo "$output"
-  echo "Please review the formatting errors before committing."
-  exit 1
-fi
-
-echo "Dataform formatting complete. Please review the changes before committing."
+dataform format
