ff47b88f942a2ac1d2225566c2acfeb2f568e2ba
Hugo Rialan <hugo.rialan@devoteamgcloud.com>
2023-06-29 15:15:19 +0200

    Clean error message

--- hooks/dataform_format.sh
+++ hooks/dataform_format.sh
@@ -2,7 +2,7 @@
 
 # Check if dataform command is available
 if ! command -v dataform >/dev/null 2>&1; then
-  echo "Error: dataform command not found. Make sure it is installed and available in PATH."
+  echo "Error: dataform command not found. Make sure it is installed. Refer to the official Dataform documentation for installation instructions: https://cloud.google.com/dataform/docs/use-dataform-cli#install_cli"
   exit 1
 fi
 
