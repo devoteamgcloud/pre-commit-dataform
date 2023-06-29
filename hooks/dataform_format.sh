266b01f004bc6a9293e37df4b07110876812e87d
Hugo Rialan <hugo.rialan@devoteamgcloud.com>
2023-06-29 13:16:12 +0200

    fix

--- hooks/dataform_format.sh
+++ hooks/dataform_format.sh
@@ -6,7 +6,16 @@ if ! command -v dataform >/dev/null 2>&1; then
   exit 1
 fi
 
-dataform format
+# Run Dataform formatting
+output=$(dataform format 2>&1)
+exit_code=$?
+
+if [ $exit_code -ne 0 ]; then
+  echo "Dataform formatting failed:"
+  echo "$output"
+  echo "Please review the formatting errors before committing."
+  exit 1
+fi
 
 echo "Dataform formatting complete. Please review the changes before committing."
 exit 0
