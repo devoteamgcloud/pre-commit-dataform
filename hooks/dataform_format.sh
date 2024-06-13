3925a748e27b963cbe7abdd7d17260416c5d8538
Hugo Rialan <hugo.rialan@devoteamgcloud.com>
2024-06-13 15:04:42 +0200

    Add automation to install Dataform CLI

--- hooks/dataform_format.sh
+++ hooks/dataform_format.sh
@@ -2,8 +2,8 @@
 
 # Check if dataform command is available, if not install it
 if ! command -v dataform >/dev/null 2>&1; then
-  echo "Dataform is not installed. Make sure NPM is installed on your machine."
-  echo "Installing Dataform now..."
+  echo "Dataform is not installed."
+  echo "Trying to install Dataform..."
   npm i -g @dataform/cli@latest
   echo "Dataform installed successfully."
 fi
