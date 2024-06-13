db3888dadccf8c783d6aa7003b69f575662c5276
Hugo Rialan <hugo.rialan@devoteamgcloud.com>
2024-06-13 14:47:29 +0200

    Add Dataform installation

--- hooks/dataform_format.sh
+++ hooks/dataform_format.sh
@@ -4,6 +4,7 @@
 if ! command -v dataform >/dev/null 2>&1; then
   echo "Dataform is not installed. Installing it now..."
   npm i -g @dataform/cli@latest
+  echo "Dataform installed successfully."
 fi
 
 # Run Dataform formatting for SQLX files
